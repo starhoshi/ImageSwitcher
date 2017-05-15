20aff83b6d92e038b678f0679458edd41353fb26
starhoshi <kensuke1751@gmail.com>
2017-05-15 11:00:49 +0900

    add storage permission.

--- options.js
+++ options.js
@@ -1,4 +1,3 @@
-// Saves options to chrome.storage
 function save_options() {
     var color = document.getElementById('color').value;
     var likesColor = document.getElementById('like').checked;
@@ -15,18 +14,12 @@ function save_options() {
     });
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
 function restore_options() {
-    // Use default value color = 'red' and likesColor = true.
-    chrome.storage.sync.get({
-        favoriteColor: 'red',
-        likesColor: true
-    }, function (items) {
-        document.getElementById('color').value = items.favoriteColor;
-        document.getElementById('like').checked = items.likesColor;
+    chrome.storage.sync.get(null, function (items) {
+        console.log(items);
     });
 }
+
 document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+document.getElementById('status').addEventListener('click', save_options);
+
