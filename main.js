9cb986212018125dd7493b700fc43d1a023cb903
starhoshi <kensuke1751@gmail.com>
2017-05-18 00:32:56 +0900

    set default icon.

--- main.js
+++ main.js
@@ -3,13 +3,13 @@
     console.log(document.querySelectorAll('img'));
     document.querySelectorAll('img').forEach(function (img) {
         if (img.src.startsWith("https://avatars3.githubusercontent.com/u/1906512")) {
-            img.src = "https://example.com";
+            img.src = "https://raw.githubusercontent.com/starhoshi/ImageSwitcher/master/icons/icon128.png";
         }
         if (img.src.startsWith("https://avatars2.githubusercontent.com/u/1906512")) {
-            img.src = "https://example.com";
+            img.src = "https://raw.githubusercontent.com/starhoshi/ImageSwitcher/master/icons/icon128.png";
         }
         if (img.src.startsWith("https://avatars1.githubusercontent.com/u/1906512")) {
-            img.src = "https://example.com";
+            img.src = "https://raw.githubusercontent.com/starhoshi/ImageSwitcher/master/icons/icon128.png";
         }
     });
 }());
