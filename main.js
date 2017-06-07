a445db86bbe4da3cfcf1085db4933237b229fb8b
Yusuke Kawamoto <novogrammer@gmail.com>
2017-06-08 03:25:53 +0900

    Revert "move to switchImages function"
    This reverts commit d3959d7c4677e8c7bdb9ec81ab6b6e566a713e13.

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
 (function () {
-    function switchImages(){
-        document.querySelectorAll('img').forEach(function (img) {
-            chrome.storage.sync.get(null, function (items) {
-                if (items.data) {
-                    items.data.forEach(function (switcher) {
-                        if (img.src.startsWith(switcher.from)) {
-                            img.src = switcher.to;
-                        }
-                    });
-                }
-            });
+    document.querySelectorAll('img').forEach(function (img) {
+        chrome.storage.sync.get(null, function (items) {
+            if (items.data) {
+                items.data.forEach(function (switcher) {
+                    if (img.src.startsWith(switcher.from)) {
+                        img.src = switcher.to;
+                    }
+                });
+            }
         });
-    }
-    switchImages();
+    });
 }());
 
