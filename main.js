190aaec827a50d4009744aa18c40bb07f1372498
Yusuke Kawamoto <novogrammer@gmail.com>
2017-06-08 11:15:01 +0900

    Switch the image partially.

--- main.js
+++ main.js
@@ -1,29 +1,55 @@
 (function () {
-    function switchImages() {
-        document.querySelectorAll('img').forEach(function (img) {
-            chrome.storage.sync.get(null, function (items) {
-                if (items.data) {
-                    items.data.forEach(function (switcher) {
-                        if (img.src.startsWith(switcher.from)) {
-                            img.src = switcher.to;
-                        }
-                    });
-                }
-            });
+    function switchImage(img) {
+        chrome.storage.sync.get(null, function (items) {
+            if (items.data) {
+                items.data.forEach(function (switcher) {
+                    if (img.src.startsWith(switcher.from)) {
+                        img.src = switcher.to;
+                    }
+                });
+            }
         });
     }
-
-    var lastTime = new Date().getTime();
-    var lastTimeDiff = 2000;
-    var mutationObserver = new MutationObserver(function () {
-        var date = new Date().getTime();
-        if ((date - lastTime) > lastTimeDiff) {
-            lastTime = date;
-            switchImages();
-        }
+    function switchAllImages() {
+        document.querySelectorAll('img').forEach(function (img) {
+            switchImage(img);
+        });
+    }
+    switchAllImages();
+    var mutationObserver = new MutationObserver(function (mutationRecordsList) {
+        mutationRecordsList.forEach(function (mutationRecord) {
+            switch(mutationRecord.type){
+                case "childList":
+                mutationRecord.addedNodes.forEach(function (addedNode) {
+                    if(addedNode instanceof HTMLElement) {
+                        addedNode.querySelectorAll('img').forEach(function (img) {
+                            switchImage(img);
+                        });
+                    }
+                });
+                break;
+                case "attributes":
+                if(mutationRecord.target instanceof HTMLElement) {
+                    if(mutationRecord.target.tagName.toLowerCase()==="img") {
+                      switchImage(mutationRecord.target);
+                    }
+                }
+                break;
+                default:
+                //DO NOTHING
+                break;
+            }
+        });
     });
 
-    switchImages();
     var body = document.querySelector('body');
-    mutationObserver.observe(body, {childList: true, subtree: true});
+    var config={
+        childList: true,
+        subtree: true,
+        attributes:true,
+        attributeFilter:["src"],
+    };
+    mutationObserver.observe(body, config);
+
+
 }());
